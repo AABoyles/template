8a02d1ea3949e82ba7439a50ac5d8cb72ee7dc35
Tony Boyles <Anthony@Boyles.cc>
2019-08-07 09:38:39 -0400

    Cleanup

--- src/template.js
+++ src/template.js
@@ -1,1 +1,12 @@
-../index.js
+(function (root, factory){
+  if(typeof define === 'function' && define.amd){
+    define([], function(){ return (root.template = factory()) });
+  } else if(typeof module === 'object' && module.exports){
+    module.exports = factory();
+  } else {
+    root.template = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function(){
+  //Insert your stuff here.
+  return {};
+}));
